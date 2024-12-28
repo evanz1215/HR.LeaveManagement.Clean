2b7c7dea255d60b73414ce24f93c36adce40eed8
Evan <evanz1215@gmail.com>
2024-12-29 07:38:01 +0800

    加入專案檔案。

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+public record GetLeaveTypeDetailsQuery(int Id) : IRequest<LeaveTypeDetailDto>;
+
+//public class GetLeaveTypeDetailsQuery : IRequest<LeaveTypeDetailDto>
+//{
+//    public int Id { get; }
+
+//    public GetLeaveTypeDetailsQuery(int id)
+//    {
+//        Id = id;
+//    }
+//}
