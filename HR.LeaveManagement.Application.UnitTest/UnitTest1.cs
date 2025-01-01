d4ee59b590b507a9a43f1f39a05d11675d088982
Evan <evanz1215@gmail.com>
2025-01-02 04:47:18 +0800

    Application Testing

---
+++ HR.LeaveManagement.Application.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace HR.LeaveManagement.Application.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ HR.LeaveManagement.Persistence.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace HR.LeaveManagement.Persistence.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
